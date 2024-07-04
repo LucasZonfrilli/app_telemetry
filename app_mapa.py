8c5adb5c237f8b521e2b4ebd08d6b2fc80dbad24
LucasZonfrilli <105242452+LucasZonfrilli@users.noreply.github.com>
2024-07-04 20:53:15 -0300

    Add files via upload

--- app_mapa.py
+++ app_mapa.py
@@ -3,32 +3,32 @@ import folium
 from folium.plugins import HeatMap
 import streamlit as st
 
-# Caminho para o arquivo CSV na pasta 'api_telemetry'
-file_path = 'D:/DADOS/Desktop/project/api_telemetry/pressao_corte.csv'
-
-# Carregar os dados
-data = pd.read_csv(file_path, delimiter=';')
-
 # Função principal que será executada pelo Streamlit
 def main():
     st.title('Mapa Pressão do Corte de Base Georreferenciado')
 
-    # Mostrar prévia dos dados
-    st.write("Prévia dos Dados:", data[['LAT', 'LON', 'VALUE']].head())
+    # Carregar o arquivo CSV usando o uploader do Streamlit
+    uploaded_file = st.file_uploader("Escolha o arquivo CSV", type="csv")
 
-    # Criar o mapa centrado nas médias de LAT e LON
-    m = folium.Map(location=[data['LAT'].mean(), data['LON'].mean()], zoom_start=11)
+    if uploaded_file is not None:
+        # Carregar os dados
+        data = pd.read_csv(uploaded_file, delimiter=';')
 
-    # Gerar os dados para o mapa de calor
-    heat_data = [[row['LAT'], row['LON'], row['VALUE']] for index, row in data.iterrows()]
+        # Mostrar prévia dos dados
+        st.write("Prévia dos Dados:", data[['LAT', 'LON', 'VALUE']].head())
 
-    # Adicionar o mapa de calor ao mapa
-    HeatMap(heat_data).add_to(m)
+        # Criar o mapa centrado nas médias de LAT e LON
+        m = folium.Map(location=[data['LAT'].mean(), data['LON'].mean()], zoom_start=11)
 
-    # Usar o componente folium_static para renderizar o mapa no Streamlit
-    from streamlit_folium import folium_static
-    folium_static(m)
+        # Gerar os dados para o mapa de calor
+        heat_data = [[row['LAT'], row['LON'], row['VALUE']] for index, row in data.iterrows()]
+
+        # Adicionar o mapa de calor ao mapa
+        HeatMap(heat_data).add_to(m)
+
+        # Usar o componente folium_static para renderizar o mapa no Streamlit
+        from streamlit_folium import folium_static
+        folium_static(m)
 
 if __name__ == "__main__":
     main()
-    
